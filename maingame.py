f34cfb10294cebe8c396ccf750d4b42cdb606325
TonyPescy <tpescy16@gmail.com>
2023-10-21 13:13:20 -0400

    Update maingame.py

--- maingame.py
+++ maingame.py
@@ -1,5 +1,5 @@
 #######################################################################################################################################
-# Title: Shanni's Sanctuary
+# Title: Maingame
 # Date Started: 2/22/2022 dropped after 2/25/2022, picked again on 10/21/23
 # Date Completed: TBD
 # Author: Tony Pescatore and Nick Pescatore
