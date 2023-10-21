88771379d3daa0c838ce57b713d72126464c20da
NAP1313 <134617896+NAP1313@users.noreply.github.com>
2023-10-21 13:15:21 -0400

    Update Character.py
    Header Added

--- Character.py
+++ Character.py
@@ -1,1 +1,8 @@
+#######################################################################################################################################
+# Title: Shanni's Sanctuary
+# Date Started: 2/22/2022 dropped after 2/25/2022, picked again on 10/21/23
+# Date Completed: TBD
+# Author: Tony Pescatore and Nick Pescatore
+# Description: Character Class to define the player character.
+#######################################################################################################################################
 
